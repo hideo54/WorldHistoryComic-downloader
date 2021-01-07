d86715744a7f3e22951fe7194dfbf2e7fed2748a
hideo54 <hideo54@hideo54.com>
2021-01-07 22:56:08 +0900

    Get volumes

--- index.ts
+++ index.ts
@@ -1,1 +1,22 @@
-console.log('Hello, world!');
+import axios from 'axios';
+import cheerio from 'cheerio';
+
+const origin = 'https://sereki-km3.shogakukan.co.jp';
+
+const getVolumes = async () => {
+    const result = await axios.get(origin);
+    const $ = cheerio.load(result.data);
+    const volumes = $('.thumbnail > a').map((i, e) => ({
+        title: e.attribs.title,
+        path: e.attribs.href.replace(origin, ''),
+    })).toArray();
+    return new Object(volumes) as {
+        title: string;
+        path: string;
+    }[];
+};
+
+(async () => {
+    const volumes = await getVolumes();
+    console.log(volumes);
+})();
