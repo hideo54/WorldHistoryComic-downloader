0c7c34abd904fa953ff2a5f916f6ed803c1f7769
hideo54 <hideo54@hideo54.com>
2021-01-07 23:24:27 +0900

    Finish creating the downloader!

--- index.ts
+++ index.ts
@@ -1,7 +1,14 @@
 import axios from 'axios';
 import cheerio from 'cheerio';
+import fs from 'fs/promises';
 
 const origin = 'https://sereki-km3.shogakukan.co.jp';
+
+const sleep = (second: number) => {
+    return new Promise<void>(resolve => {
+        setTimeout(resolve, second * 1000);
+    });
+};
 
 const getVolumes = async () => {
     const result = await axios.get(origin);
@@ -16,7 +23,37 @@ const getVolumes = async () => {
     }[];
 };
 
+const downloadBookPageImages = async (bookPath: string) => {
+    const result = await axios.get<{
+        slug: string;
+        title: string;
+        contents: {
+            sort_no: number;
+            mime_type: string;
+            width: string; // numeric
+            height: string; //numeric
+        }[];
+    }>(`${origin}${bookPath}/json`);
+    const pageData = result.data;
+    const slug = pageData.slug;
+    await fs.mkdir(`dist/${slug}`);
+    for (const page of pageData.contents) {
+        const i = page.sort_no;
+        const url = `${origin}/contents/${slug}/${i}/base64`;
+        const base64 = (await axios.get(url)).data as string;
+        const data = Buffer.from(base64.replace('data:image/jpeg;base64,', ''), 'base64');
+        await fs.writeFile(`dist/${slug}/${i}.jpg`, data);
+        await sleep(0.5);
+        console.log(`Downloaded p. ${i} / ${pageData.contents.length}`);
+    }
+};
+
 (async () => {
     const volumes = await getVolumes();
-    console.log(volumes);
+    let count = 0;
+    for (const volume of volumes) {
+        count += 1;
+        console.log(`Start downloading Vol. ${count} / ${volumes.length}`);
+        await downloadBookPageImages(volume.path);
+    }
 })();
